3d21ae4f82f3958885db5cf55949f967d937c11a
Daniel Hauschildt <daniel.hauschildt@9elements.com>
2015-12-30 15:34:40 +0100

    Spec bugfix

--- spec/lib/web_hook_spec.rb
+++ spec/lib/web_hook_spec.rb
@@ -18,21 +18,21 @@ describe IX::WebHook do
 
   describe '#delivery_method' do
     describe 'when faraday' do
-      let(delivery_method) { :faraday }
+      let(:delivery_method) { :faraday }
       subject {
         IX::WebHook.delivery_method = delivery_method
       }
       it { is_expected.to be_an(IX::WebHook::DeliveryMethod::Faraday) }
     end
     describe 'when console' do
-      let(delivery_method) { :console }
+      let(:delivery_method) { :console }
       subject {
         IX::WebHook.delivery_method = delivery_method
       }
       it { is_expected.to be_an(IX::WebHook::DeliveryMethod::Console) }
     end
     describe 'when test' do
-      let(delivery_method) { :test }
+      let(:delivery_method) { :test }
       subject {
         IX::WebHook.delivery_method = delivery_method
       }
